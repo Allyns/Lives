4be8de009cebfdd1a762aea612a6b932de6866a3
[allyns] <1839565349@qq.com>
2016-03-25 09:57:45 +0800

    add MainApp

---
+++ app/src/main/java/com/allyn/lives/MainApp.java
@@ -0,0 +1,13 @@
+package com.allyn.lives;
+
+import android.test.mock.MockApplication;
+
+/**
+ * Created by Administrator on 2016/3/25.
+ */
+public class MainApp extends MockApplication{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
