708d968a6b6bbf550d53bf2085b86db13b5191c7
[allyns] <1839565349@qq.com>
2016-04-11 17:56:43 +0800

    update  add myview

--- app/src/main/java/com/allyn/lives/fragment/TVFragment.java
+++ app/src/main/java/com/allyn/lives/fragment/TVFragment.java
@@ -7,6 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.allyn.lives.R;
+import com.allyn.lives.view.MyView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2016/3/22.
@@ -18,10 +23,21 @@ public class TVFragment extends BaseFragment {
         return fragment;
     }
 
+    @Bind(R.id.myView)
+    MyView mmyView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_tv, container, false);
+        View v = inflater.inflate(R.layout.activity_my_view, container, false);
+        ButterKnife.bind(this, v);
+        mmyView.startAnim(4000);
         return v;
+    }
+
+    @OnClick(R.id.button)
+    public void start(View v) {
+        mmyView.stopAnim();
+        mmyView.startAnim(4000);
     }
 }
---
+++ app/src/main/java/com/allyn/lives/ui/MyViewActivity.java
@@ -0,0 +1,23 @@
+package com.allyn.lives.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.allyn.lives.R;
+import com.allyn.lives.view.MyView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MyViewActivity extends AppCompatActivity {
+    @Bind(R.id.myView)
+    MyView mmyView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+        setContentView(R.layout.activity_my_view);
+        mmyView.startAnim(1000);
+    }
+}
