87247da65f1c5573e9ba8f431af78e9587d67184
Allyns <1839565349@qq.com>
2016-04-21 15:57:01 +0800

    add baseAdapter

---
+++ app/src/main/java/com/allyn/lives/view/base/SectionEntity.java
@@ -0,0 +1,19 @@
+package com.allyn.lives.view.base;
+
+public  class SectionEntity<T> {
+    public boolean isHeader;
+    public T t;
+    public String header;
+
+    public SectionEntity(boolean isHeader, String header) {
+        this.isHeader = isHeader;
+        this.header = header;
+        this.t = null;
+    }
+
+    public SectionEntity(T t) {
+        this.isHeader = false;
+        this.header = null;
+        this.t = t;
+    }
+}
