6cf741a1d1fb72cc8eb1b92a5b1227cf493b762b
“allyns” <1839565349@qq.com>
2016-04-29 21:21:42 +0800

    update view pankage

--- app/src/main/java/com/allyn/lives/MainApp.java
+++ app/src/main/java/com/allyn/lives/MainApp.java
@@ -6,6 +6,9 @@ import android.test.mock.MockApplication;
  * Created by Administrator on 2016/3/25.
  */
 public class MainApp extends MockApplication{
+
+
+
     @Override
     public void onCreate() {
         super.onCreate();
