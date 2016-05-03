26f74d478617aefc322f933ecdd16f8d1dadf6c4
Allyns <1839565349@qq.com>
2016-05-03 17:12:56 +0800

    add view widgets

--- app/src/main/java/com/allyn/lives/adapter/ListAdapter.java
+++ app/src/main/java/com/allyn/lives/adapter/ListAdapter.java
@@ -31,23 +31,3 @@ public class ListAdapter extends RecyclerView.Adapter{
     }
 
 }
-
-//public class QuickAdapter extends BaseQuickAdapter<Status> {
-//    public QuickAdapter(Context context) {
-//        super(context, R.layout.tweet, DataServer.getSampleData(100));
-//    }
-//
-//    public QuickAdapter(Context context, int dataSize) {
-//        super(context, R.layout.tweet, DataServer.getSampleData(dataSize));
-//    }
-//
-//    @Override
-//    protected void convert(BaseViewHolder helper, Status item) {
-//        helper.setText(R.id.tweetName, item.getUserName())
-//                .setText(R.id.tweetText, item.getText())
-//                .setText(R.id.tweetDate, item.getCreatedAt())
-//                .setImageUrl(R.id.tweetAvatar, item.getUserAvatar())
-//                .setVisible(R.id.tweetRT, item.isRetweet())
-//                .linkify(R.id.tweetText);
-//    }
-//}
