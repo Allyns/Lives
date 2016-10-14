0a383ba60b8f63fbca619a0e00454538d8dc22bc
Allyns <1839565349@qq.com>
2016-10-14 14:52:05 +0800

    添加一个漂亮的详情界面

--- app/src/main/java/com/allyn/lives/fragment/SettingsFragment.java
+++ app/src/main/java/com/allyn/lives/fragment/SettingsFragment.java
@@ -1,14 +1,16 @@
 package com.allyn.lives.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.allyn.lives.R;
+import com.allyn.lives.activity.ConstMainActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -22,12 +24,21 @@ public class SettingsFragment extends Fragment {
         return fragment;
     }
 
+    @Bind(R.id.button)
+    Button button;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_settings, container, false);
         ButterKnife.bind(this, v);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().startActivity(new Intent(getActivity(), ConstMainActivity.class));
+
+            }
+        });
         return v;
     }
 
